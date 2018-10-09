1cfa44d4740bf72e141ffc46d87197c9877b04d8
Sandy0516 <42035028+Sandy0516@users.noreply.github.com>
2018-10-09 19:38:54 +0530

    Added ButtonHandlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,6 +6,13 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
 
+    showNextQuestion(){
+        this.setState((state) => {
+            return {quiz_position: state.quiz_position + 1}
+        })
+
+    }
+
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 }
@@ -15,7 +22,8 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                    showNextQuestionHandler = {this.showNextQuestion.bind(this)}/>}
             </div>
         )
     }
