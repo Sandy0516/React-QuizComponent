ce949afc4dd8d253cfbcfcc3c4481f5111bcb454
Sandy0516 <42035028+Sandy0516@users.noreply.github.com>
2018-10-09 18:49:35 +0530

    QuizEnd paragraph text is changed
    paragaraph text is changed and formated all classes

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,14 +1,13 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class QuizEnd extends Component{
-    render(){
-        return(
+class QuizEnd extends Component {
+    render() {
+        return (
             <div>
-                <p>Thanks for Playing!</p>
+                <p>Thanks for playing!</p>
                 <a href=''>Reset Quiz</a>
             </div>
         )
     }
 }
-
 export default QuizEnd
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class QuizQuestionButton extends Component{
-    render(){
-        return(
-            
-                <li>
-                    <button>{this.props.button_text}</button>
-                </li>
-            
-        )
+class QuizQuestionButton extends Component {
+    render() {
+        return (
+            <li>
+                <button>{this.props.button_text}</button>
+            </li>
+       )
     }
 }
 
